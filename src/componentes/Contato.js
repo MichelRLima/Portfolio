9a9f2fa91cbb158600b87f5254bb95e35bc9f0fa
MichelRLima <michelrocha502@gmail.com>
2023-04-29 19:02:51 -0300

    contatos

--- src/componentes/Contato.js
+++ src/componentes/Contato.js
@@ -1,8 +1,31 @@
+import styles from './contato.module.css'
+import { AiOutlineMail, AiOutlineWhatsApp, AiFillLinkedin } from 'react-icons/ai';
+
 function Contato (){
 
     return(
     <>
-    <h1>Contato</h1>
+    <h2 className={styles.titulo}>Contato</h2>
+
+    <div className={styles.containerContato}>
+         <div className={styles.contato}>
+            <AiOutlineMail size={40} /> 
+            <p>michelrocha502@gmail.com</p>
+         </div>
+
+         <div className={styles.contato}>
+            <AiOutlineWhatsApp color='#34af23' size={40} /> 
+           
+            <p>+55 84 99812-0957</p>
+         </div>
+
+         <div className={styles.contato}>
+            <AiFillLinkedin color='#0e76a8' size={40} /> 
+           
+            <p><a href='https://www.linkedin.com/in/michel-rocha-01b550210/' target='_blanck'>Michel Rocha</a></p>
+         </div>
+    </div>
+    
     </>
     )
 }
