244ef6516cd9758b9e4d07eea501f9a17959fbce
MichelRLima <michelrocha502@gmail.com>
2023-04-29 23:45:26 -0300

    curriculo

--- src/componentes/CV.js
+++ src/componentes/CV.js
@@ -1,9 +1,21 @@
+import styles from './cv.module.css'
+import { AiOutlineCloudDownload } from 'react-icons/ai';
 function CV (){
-
+    const fileUrl = "./arquivos/teste.txt";
+    const fileName = "teste.txt";
 
     return (
         <>
-        <h1>currículo</h1>
+      
+        <h3 className={styles.descricao}>Clica que baixar meu Currículo</h3>
+        
+        
+        <div className={styles.containerDown}>
+        
+        <a  href={fileUrl} download={fileName} className={styles.down}><AiOutlineCloudDownload size={90}/></a>
+        <p>Download</p>
+        </div>
+       
         </>
     )
 }
